85fa12ba150cc1c80b4b207068bd7f08f1131a03
Andrew Bertram <andrew.bertram23@gmail.com>
2021-04-15 11:12:23 -0400

    Bonus now complete

--- src/bonus/Bonus.js
+++ src/bonus/Bonus.js
@@ -1,6 +1,6 @@
-// BONUS
+// // BONUS
 
-// 6a
+// // 6a
 import { useState } from 'react';
 
 // 6b
@@ -8,17 +8,17 @@ const Bonus = () => {
     // 6d
     const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
     // 6f
-    const [letter, addLetter] = useState(letters[0]);
-    const nextLetter = () => 
+    const [letter, setLetter] = useState(0);
+    // const nextLetter = () =>
 
     // 6e
     return (
         <div>
-            <h1>{letter}</h1>
-            <button>NEXT LETTER</button>
+            <h1>{letters[letter]}</h1>
+            <button onClick={() => letter < (letters.length - 1) ? setLetter(letter + 1) : setLetter(0)}>NEXT LETTER</button>
         </div>
     );
-}
+    }
 
-// 6c
-export default Bonus;
+    // 6c
+    export default Bonus;
